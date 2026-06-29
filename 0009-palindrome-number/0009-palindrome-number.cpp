e8d851184a3d5a33108faf345d208b94ddaab3e5
shashank gangwar <shashank4230g@gmail.com>
2026-06-29 23:48:07 +0530

    Time: 15 ms (6.03%), Space: 10.6 MB (20.11%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string s1= s;
+        reverse(s.begin(),s.end());
+        if(s==s1) return true;
+        else return 0;
+        
+    }
+};
