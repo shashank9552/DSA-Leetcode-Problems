8c8c0b793ada7ec9e3a500fcd3bc613a2d87ad6d
shashank gangwar <shashank4230g@gmail.com>
2026-07-07 21:20:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (51.79%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+
+        long long x = 0;
+        long long sum = 0;
+
+        string s = to_string(n);
+
+        for (char c : s) {
+            if (c != '0') {
+                int digit = c - '0';
+                x = x * 10 + digit;
+                sum += digit;
+            }
+        }
+
+        return x * sum;
+    }
+};
