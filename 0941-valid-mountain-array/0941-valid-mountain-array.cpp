bf9af2e7c7f801e10af296789ab26dad37f10f2f
shashank gangwar <shashank4230g@gmail.com>
2026-07-18 19:22:43 +0530

    Time: 22 ms (36.6%), Space: 26.3 MB (48.18%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -8,15 +8,15 @@ public:
 
         int i = 0;
 
-        // Climb up
+        // Move uphill
         while (i + 1 < n && arr[i] < arr[i + 1])
             i++;
 
-        // Peak can't be first or last
+        // Peak cannot be first or last
         if (i == 0 || i == n - 1)
             return false;
 
-        // Climb down
+        // Move downhill
         while (i + 1 < n && arr[i] > arr[i + 1])
             i++;
 
