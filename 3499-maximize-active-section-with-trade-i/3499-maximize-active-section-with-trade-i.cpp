cf8526f9774157ea4c4695370490c93c573cb672
shashank gangwar <shashank4230g@gmail.com>
2026-07-21 14:39:44 +0530

    Time: 182 ms (8.11%), Space: 175.9 MB (11.71%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int active = 0;
+        for (char c : s)
+            if (c == '1')
+                active++;
+
+        string t = "1" + s + "1";
+
+        vector<pair<char, int>> runs;
+
+        for (char c : t) {
+            if (runs.empty() || runs.back().first != c)
+                runs.push_back({c, 1});
+            else
+                runs.back().second++;
+        }
+
+        int ans = active;
+
+        for (int i = 1; i + 1 < runs.size(); i++) {
+            if (runs[i].first == '1' &&
+                runs[i - 1].first == '0' &&
+                runs[i + 1].first == '0') {
+
+                ans = max(ans,
+                          active + runs[i - 1].second + runs[i + 1].second);
+            }
+        }
+
+        return ans;
+    }
+};
